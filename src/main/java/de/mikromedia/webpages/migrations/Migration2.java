7236a321e5ac1936357917aed41e6fea8c3cf1ea
Malte Reißig <malte@mikromedia.de>
2015-11-17 17:57:50 +0100

    Do not do standard assignments (Migration2) to 'System'.

--- src/main/java/de/mikromedia/webpages/migrations/Migration2.java
+++ src/main/java/de/mikromedia/webpages/migrations/Migration2.java
@@ -18,24 +18,24 @@ public class Migration2 extends Migration {
 	@Override
 	public void run () {
 
-        // 1) siteTopic to "System" assignment
-        Topic systemWorkspace = workspacesService.getWorkspace(AccessControlService.SYSTEM_WORKSPACE_URI);
+        // 1) Assign standard site topic and assoc to "DeepaMehta" assignment
+        Topic deepaMehtaWorkspace = workspacesService.getWorkspace(WorkspacesService.DEEPAMEHTA_WORKSPACE_URI);
 		Topic siteTopic = dms.getTopic("uri", new SimpleValue("de.mikromedia.standard_site"));
-        workspacesService.assignToWorkspace(siteTopic, systemWorkspace.getId());
+        workspacesService.assignToWorkspace(siteTopic, deepaMehtaWorkspace.getId());
+
         // 2) Default "website" user association
         Topic adminTopic = accessControlService.getUsernameTopic(AccessControlService.ADMIN_USERNAME);
-        dms.createAssociation(new AssociationModel("dm4.core.association",
+        Association assoc = dms.createAssociation(new AssociationModel("dm4.core.association",
                 new TopicRoleModel(adminTopic.getId(), "dm4.core.default"),
                 new TopicRoleModel(siteTopic.getId(), "dm4.core.default")));
+        workspacesService.assignToWorkspacE(assoc, deepaMehtaWorkspace.getId());
 
-        // 3) Type workspace assignments
+        // 3) Type workspace assignments to "DeepaMehta
         // Note: Child topic types are curerntly not assigned to any workspace
 		TopicType siteType = dms.getTopicType("de.mikromedia.site");
         TopicType menuItemType = dms.getTopicType("de.mikromedia.menu.item");
         TopicType redirectType = dms.getTopicType("de.mikromedia.redirect");
         TopicType pageType = dms.getTopicType("de.mikromedia.page");
-
-        Topic deepaMehtaWorkspace = workspacesService.getWorkspace(WorkspacesService.DEEPAMEHTA_WORKSPACE_URI);
         workspacesService.assignTypeToWorkspace(siteType, deepaMehtaWorkspace.getId());
         workspacesService.assignTypeToWorkspace(menuItemType, deepaMehtaWorkspace.getId());
         workspacesService.assignTypeToWorkspace(redirectType, deepaMehtaWorkspace.getId());
